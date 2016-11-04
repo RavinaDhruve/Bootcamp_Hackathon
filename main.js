e089fa47d32759aff405323924745354eb1ab093
cshelke@indiana.edu <cshelke@indiana.edu>
2016-11-04 16:14:40 -0700

    adding legit paragraphs

--- main.js
+++ main.js
@@ -9,28 +9,16 @@ var app = express()
 /////////////////////////////////////////////////////////////////
 // REDIS - sample
 var client = redis.createClient(6379, '127.0.0.1', {});
-var para1 = "All about Elon Musk's journey of Tesla. All about Elon Musk's journey of Tesla." +
-            "All about Elon Musk's journey of Tesla. All about Elon Musk's journey of Tesla." +
-            "All about Elon Musk's journey of Tesla. All about Elon Musk's journey of Tesla." +
-            "All about Elon Musk's journey of Tesla. All about Elon Musk's journey of Tesla." +
-            "All about Elon Musk's journey of Tesla. All about Elon Musk's journey of Tesla.";
+var para1 = "Elon Reeve Musk ( born June 28, 1971) is a South African-born Canadian-American business magnate, investor, engineer and inventor. He is the founder, CEO, and CTO of SpaceX; co-founder, CEO, and product architect of Tesla Motors; co-founder and chairman of SolarCity; co-chairman of OpenAI; co-founder of Zip2; and founder of X.com which merged with PayPal of Confinity. As of June 2016, he has an estimated net worth of US$11.5 billion, making him the 83rd wealthiest person in the world.";
 client.set("get1", para1);
 //client.get("P1", function(err,value){ console.log(value)});
 
-var para2 = "All about Elon Musk's Books. All about Elon Musk's Books." +
-            "All about Elon Musk's Books. All about Elon Musk's Books." +
-            "All about Elon Musk's Books. All about Elon Musk's Books." +
-            "All about Elon Musk's Books. All about Elon Musk's Books." +
-            "All about Elon Musk's Books. All about Elon Musk's Books.";
+var para2 = "Tesla Motors (often shortened to Tesla) is an American automaker and energy storage company co-founded by Martin Eberhard, Marc Tarpenning, JB Straubel, Elon Musk and Ian Wright, and is based in Palo Alto, California. The company specializes in electric cars and their powertrain components and also produces battery charging equipment. Tesla first gained widespread attention following its production of the Tesla Roadster, the first electric sports car, in 2008. The company's second vehicle, the Model S, an electric luxury sedan, debuted in 2012. In Q1 2013, Tesla released its stock profits for the first time from its NASDAQ ticker symbol. Global sales for the Model S passed 100K units in December 2015, three and a half years after its introduction, and it was the world's best-selling plug-in vehicle that year.";
 client.set("get2", para2);
 //client.get("P2", function(err,value){ console.log(value)});
 
 
-var para3 = "All about Elon Musk's SpaceX. All about Elon Musk's SpaceX." +
-			"All about Elon Musk's SpaceX. All about Elon Musk's SpaceX." +
-			"All about Elon Musk's SpaceX. All about Elon Musk's SpaceX." +
-			"All about Elon Musk's SpaceX. All about Elon Musk's SpaceX." +
-			"All about Elon Musk's SpaceX. All about Elon Musk's SpaceX." ;
+var para3 = "Space Exploration Technologies Corporation, better known as SpaceX, is an American aerospace manufacturer and space transport services company headquartered in Hawthorne, California, United States. It was founded in 2002 by Tesla Motors CEO and former PayPal entrepreneur Elon Musk with the goal of creating the technologies to reduce space transportation costs and enable the colonization of Mars. It has developed the Falcon 1 and Falcon 9 launch vehicles, both designed to be reusable, and the Dragon spacecraft which is flown into orbit by the Falcon 9 launch vehicle to supply the International Space Station (ISS) with cargo. A manned version of Dragon is in development. SpaceX is funded by government subsidies and contracts with multiple entities." ;
 client.set("get3", para3);
 //client.get("P3", function(err,value){ console.log(value)});
 /////////////////////////////////////////////////////////////////
